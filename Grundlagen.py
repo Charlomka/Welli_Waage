daabc12aef633f878b9e79fcfa0dc40fcb146214
Charlomka <mohr.char@gmail.com>
2021-06-06 20:26:31 +0200

    class "Vogel" mit __init__ und Bobby erstellen
    Wir legen für jeden meiner Vögel eine Instance an. Start mit Bobby.

--- Grundlagen.py
+++ Grundlagen.py
@@ -1,2 +1,18 @@
+
+
+#Start 2: Eine Klasse erstellen
+class Vogel:
+    def __init__(self, name, schlupfjahr, gender, gewicht, verliebt, art):
+        self.name = name
+        self.schlupfjahr = schlupfjahr 
+        self.gender = gender
+        self.gewicht = gewicht
+        self.verliebt = verliebt
+        self.art = art 
+
+#Bobby erstellen 
+Bobby = Vogel("Bobby", 2018, "m", 52, 1, "Welli")
+
 #Start: 6 kleine und 2 große Vögel
-Wellensittiche = ["Bobby", "Klausi", "Quackie", "Olga", "Tonya", "Melek"]
+Wellensittiche = [Bobby, "Klausi", "Quackie", "Olga", "Tonya", "Melek"]
+Nymphensittiche = ["Jahari", "Zari"]
